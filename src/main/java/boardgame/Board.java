bb8ee862ea496e42317c241d7446800df1fbe29d
Lucas Leandro <lucasl_pereira@hotmail.com>
2026-01-21 12:04:02 -0300

    Layout do tabuleiro implementado

---
+++ src/main/java/boardgame/Board.java
@@ -0,0 +1,39 @@
+package boardgame;
+
+public class Board {
+
+    private int rows;
+    private int column;
+    private Piece[][] pieces;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.column = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return column;
+    }
+
+    public void setColumns(int columns) {
+        this.column = columns;
+    }
+
+
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
+}
